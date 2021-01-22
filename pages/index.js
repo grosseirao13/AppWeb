a1836c055bae87adf4cb0d19c50a2c97af900a0c
Felipe <cientistagames0.15.0@gmail.com>
2021-01-21 22:22:30 -0300

    1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 $obj = require('page1.html')
 function Home() {
     return (
-        <div>
-            <h4>canal Hey jorgie</h4>
-            <a>https://www.youtube.com/channel/UCgytNKYjZ8qni0HQfA5hqjA</a>
-        </div>
+        <h4>Canal Hey Jorgie</h4> 
         );
 }
 
