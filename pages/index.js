723bea04d411f30c2d6229521fadfcbaf11e3d67
Felipe <cientistagames0.15.0@gmail.com>
2021-01-22 03:16:37 -0300

    typos

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Link from 'next/Link'
+import Link from 'next/Link';
 
 function Home() {
     return (
         <div>
             <h1>Baiano Shop</h1>
+
             <Link href="/produlto">
                 <a>comprar rede</a>
-                </Link>
-                
-        </div >
-        )
+            </Link>
+        </div>
+    )
 }
 
 export default Home
