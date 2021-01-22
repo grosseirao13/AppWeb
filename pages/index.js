178f0c3df419f9c03c6966812189d8c9bb5a1c4e
Felipe <cientistagames0.15.0@gmail.com>
2021-01-22 03:28:43 -0300

    typos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Link from 'next/Link';
+import Link from 'next/Link'
 
 function Home() {
     return (
         <div>
-            <h1>Baiano Shop</h1>
-
             <Link href="/produlto">
                 <a>comprar rede</a>
             </Link>
